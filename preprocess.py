e676772295b9a463573268aa42bfe34a47af9649
Joshua Chin <5422226+Joshua-Chin@users.noreply.github.com>
2025-08-24 00:56:44 -0700

    Significant refactor / cleanup

---
+++ preprocess.py
@@ -0,0 +1,33 @@
+import random
+
+
+SEP = '#~#'
+
+with open('./prompts/unguided-generation.txt') as f:
+    PROMPTS_NO_TOPICS = [prompt.strip() for prompt in f]
+
+def get_as_messages(data):
+    headline = get_headline(data['text'])
+    messages = get_prompt()
+    messages.append(format_reply(headline))
+    return {'messages': messages}
+    
+def get_headline(text):
+    return text.split(SEP)[0].strip()
+
+def get_prompt():
+    return [
+        {
+            "role": "user", 
+            "content": get_prompt_no_topic(),
+        }
+    ]
+    
+def get_prompt_no_topic():
+    return random.choice(PROMPTS_NO_TOPICS)
+
+def format_reply(headline):
+    return {
+        "role": "assistant",
+        "content": headline,
+    }
